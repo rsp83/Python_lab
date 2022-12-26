aeacb0411f604b959228b2ddb2949b686193f73d
rsp83 <110043578+rsp83@users.noreply.github.com>
2022-12-26 13:01:16 +0000

    ajustes

---
+++ exercise_1.py
@@ -0,0 +1,18 @@
+'''
+exercise_1
+Calculate the area of a rectangle:
+Write a Python program that asks the user to enter the width and height of a rectangle
+and then calculates and prints the area of the rectangle
+'''
+
+#get the user to enter the width of a rectangle
+width = input("What is the width ?")
+print("Good, the width is " + width )
+
+#get the user to enter the height of a rectangle
+height = input("What is the height ?")
+print("Good, the height is " + height )
+
+
+
+
