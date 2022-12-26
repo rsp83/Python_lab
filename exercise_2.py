4ddc6fd5165d127a04bfa47be9ab78b2e4e4f8cf
rsp83 <110043578+rsp83@users.noreply.github.com>
2022-12-26 14:55:12 +0000

    ajustes

--- exercise_2.py
+++ exercise_2.py
@@ -3,4 +3,11 @@
      Write a Python program that asks the user to enter a temperature in Celsius
       and then converts and prints the temperature in Fahrenheit."""
 
-      
+
+#asks the user to enter a temperature in Celsius
+celsius = input("Hello!, what is the temperature now in Celsius ?")
+
+#converts and prints the temperature in Fahrenheit
+convertion = celsius * 9/5 + 32
+print("the value of the entered value of Celsius + \
+ is converted to the following in Fahrenheit :" +convertion+ "thank you for using this proggramm")
