326a8a2f76bd471796e6d414e30da3d961133a8e
rsp83 <110043578+rsp83@users.noreply.github.com>
2022-12-27 19:19:04 +0000

    ajustes

--- exercise_7.py
+++ exercise_7.py
@@ -3,29 +3,34 @@
      Write a Python program that asks the user to enter a list of numbers
      and then calculates and prints the average of those numbers"""
 #declare the function
-def average(numbers):
+def calculate_average(function_numbers):
+    """Calculates the average of a list of numbers.
+
+    Args:
+    numbers (list): A list of numbers to calculate the average of.
+
+    Returns:
+    float: The average of the numbers."""
 
 #Initialize a variable to store the sum
-    sum = 0
+    total_sum = 0
 
 
 #iterate over the elements of the list
-    for number in numbers:
+    for number in function_numbers:
         #add the current element to the sum
-        sum += number
+        total_sum += number
 #calculate the average by dividing the sum by the lenght of the list
-    average = sum / len(numbers)
+    average = total_sum / len(function_numbers)
 
     return average
 
 
 #get the list
-numbers = input("enter a list of numbers separated by space")
+input_numbers = input("enter a list of numbers separated by space")
 
 #separate the numbers into a list
-numbers = [int(x) for x in numbers.split()]
+input_numbers = [int(x) for x in input_numbers.split()]
 
-avg = average(numbers)
+avg = calculate_average(input_numbers)
 print (f"the average of the numbers is: {avg}")
-
-
